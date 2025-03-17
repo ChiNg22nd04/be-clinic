3b6246e84d4e6729afa1c4073ebddc0765ed055e
ChiNg22nd04 <dedonai99@gmail.com>
2025-03-17 17:21:19 +0700

    fix connect database

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("database_name", "username", "password", {
+const sequelize = new Sequelize("ClinicManagement16th3_P3", "sa", "NewPassword@123", {
 	host: "localhost",
 	dialect: "mssql", // Sử dụng SQL Server
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Kết nối thành công đến database!");
+  })
+  .catch((err) => {
+    console.error("Kết nối thất bại:", err);
+  });
 
 module.exports = sequelize;
