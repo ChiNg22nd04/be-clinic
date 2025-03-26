87b39e959f954c24ac236b7832bc00acd90d9a48
ChiNg22nd04 <dedonai99@gmail.com>
2025-03-26 22:15:21 +0700

    fix bug date appointment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,14 +3,63 @@ const Appointments = require("../models/appointments.model");
 const { DATE, DataTypes } = require("sequelize");
 const moment = require("moment");
 
+const createPatientProfile = async (patientId) => {
+	try {
+		const [paitent] = await sequelize.query(
+			`SELECT * FROM [PatientProfile] WHERE patient_id = :patientId`,
+			{
+				replacements: { patientId },
+				type: sequelize.QueryTypes.SELECT,
+			}
+		);
+		console.log(paitent);
+		if (!paitent) {
+			await sequelize.query(
+				`INSERT INTO [PatientProfile] (patient_id, date_of_birth, gender, address)
+				VALUES (:patientId, :dateOfBirth, :gender, :address);`,
+				{
+					replacements: {
+						patientId: patientId,
+						dateOfBirth: null,
+						gender: 2,
+						address: null,
+					},
+					type: sequelize.QueryTypes.INSERT,
+				}
+			);
+			console.log(`PatientProfile created for user ID: ${patientId}`);
+		}
+	} catch (err) {
+		console.error("Error ensuring PatientProfile exists:", err);
+		throw new Error("Error creating patient profile.");
+	}
+};
 const scheduleAppointment = async (req, res) => {
 	try {
-		const { doctorId, symptoms, appointmentDate } = req.body;
-		const patientId = req.user.id;
+		const { doctorId, appointmentDate, symptoms } = req.body;
+		console.log(req.body);
+		if (!doctorId || !appointmentDate)
+			return res.status(400).json({ message: "Please fill in the required fields" });
 
-		if (!doctorId || !appointmentDate) {
-			return res.status(400).json({ message: "doctorId và appointmentDate là bắt buộc" });
+		// Kiểm tra định dạng ngày
+		const formattedDateString = moment(
+			appointmentDate,
+			["YYYY-M-D", "YYYY-MM-DD"],
+			true
+		).format("YYYY-MM-DD");
+		const isDate = moment(formattedDateString, "YYYY-MM-DD", true);
+
+		console.log("formattedDateString", formattedDateString);
+		console.log("isDate", isDate.isValid());
+
+		if (!isDate.isValid()) {
+			return res.status(400).json({ message: "Invalid appointment date." });
 		}
+		const formattedDate = moment(formattedDateString).format("YYYY-MM-DD HH:mm:ss");
+
+		const patientId = req.user.id;
+		console.log("patientId", patientId);
+		await createPatientProfile(patientId);
 
 		const [doctor] = await sequelize.query(
 			`SELECT * FROM [DoctorProfile] WHERE doctor_id = :doctorId`,
@@ -19,26 +68,18 @@ const scheduleAppointment = async (req, res) => {
 				type: sequelize.QueryTypes.SELECT,
 			}
 		);
-
+		console.log(doctor);
 		if (!doctor) return res.status(404).json({ message: "Doctor not found." });
-		const formattedDate = moment(appointmentDate).format("YYYY-MM-DD HH:mm:ss");
 
-		// Chuyển đổi appointmentDate thành kiểu Date
-		// const formattedDate = new Date(appointmentDate);
-		if (isNaN(formattedDate)) {
-			return res.status(400).json({ message: "Ngày hẹn không hợp lệ" });
-		}
-
-		// Thêm lịch hẹn vào database
 		await sequelize.query(
 			`INSERT INTO [Appointments] (patient_id, doctor_id, symptoms, appointment_date, status)
-			VALUES (:patientId, :doctorId, :symptoms, :appointmentDate, :status);`,
+		VALUES (:patientId, :doctorId, :symptoms, :appointmentDate, :status);`,
 			{
 				replacements: {
-					patientId,
+					patientId: patientId,
 					doctorId: doctor.doctor_id,
 					symptoms: symptoms || null,
-					appointmentDate: formattedDate, // Đảm bảo là kiểu Date
+					appointmentDate: formattedDate,
 					status: 0,
 				},
 				type: sequelize.QueryTypes.INSERT,
@@ -46,7 +87,7 @@ const scheduleAppointment = async (req, res) => {
 		);
 
 		res.status(201).json({
-			message: "Đặt lịch thành công",
+			message: "Appointment successful ",
 			appointment: {
 				patientId,
 				doctorId: doctor.doctor_id,
@@ -55,9 +96,9 @@ const scheduleAppointment = async (req, res) => {
 				status: 0,
 			},
 		});
-	} catch (error) {
-		console.error("Lỗi khi đặt lịch khám:", error);
-		res.status(500).json({ message: "Lỗi máy chủ, vui lòng thử lại sau" });
+	} catch (err) {
+		console.error("Error in making appointment:", err);
+		res.status(500).json({ message: "Server error, please try again later." });
 	}
 };
 
