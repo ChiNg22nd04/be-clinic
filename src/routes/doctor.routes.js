a6a3ebd5f36b71377d7352a0c9ba1861ed8e24ef
ChiNg22nd04 <dedonai99@gmail.com>
2025-04-17 19:45:22 +0700

    create func get prescription for doctor

--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -2,23 +2,21 @@ const express = require("express");
 const { verifyToken, authorizeRole } = require("../middleware/authJwt");
 const {
 	getExaminationForm,
-	getListExaminationForm,
+	getAllExaminationForm,
 	updateExaminationForm,
 	updatePrescription,
+	getPrescription,
 } = require("../controllers/doctor.controller");
 
 const router = express.Router();
 // API đặt lịch khám bệnh
-router.get("/medical-examination/get-all", verifyToken, authorizeRole(1), getListExaminationForm);
+router.get("/medical-examination/get-all", verifyToken, authorizeRole(1), getAllExaminationForm);
 router.get("/medical-examination/get-detail", verifyToken, authorizeRole(1), getExaminationForm);
-router.put(
-	"/medical-examination/update-form",
-	verifyToken,
-	authorizeRole(1),
-	updateExaminationForm
-);
+router.put("/medical-examination/update", verifyToken, authorizeRole(1), updateExaminationForm);
 
 router.post("/prescription/update-form", verifyToken, authorizeRole(1), updatePrescription);
+router.post("/prescription/form", verifyToken, authorizeRole(1), getPrescription);
+
 router.get("/test", (req, res) => {
 	console.log("✅ Test API called!");
 	res.send("API hoạt động!");
