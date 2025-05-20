bc07c3b36a356f13e9c6a85e1634e4011c77a26f
ChiNg22nd04 <dedonai99@gmail.com>
2025-05-20 12:04:17 +0700

    Remove getArticlesByID, create getAllAchievements and adjusted SQL queries to filter by category. Updated routes to reflect the new endpoint for achievements.

--- src/routes/common.routes.js
+++ src/routes/common.routes.js
@@ -12,14 +12,15 @@ router.get("/clinics", CommonController.getAllClinics);
 router.post("/specialties/clinic-id", CommonController.getSpecialtiesByIDClinic);
 
 router.post("/specialties/doctor-all", CommonController.getAllSpecialtiesDoctor);
- 
+
 router.post("/shifts-all", CommonController.getAllShiftDoctor);
 
 router.post("/shifts/doctor-id", CommonController.getShiftByIDDoctor);
 
 router.get("/articles", CommonController.getAllArticles);
- 
-router.get("/articles/articles-id", CommonController.getArticlesByID);
+
+router.get("/achievements", CommonController.getAllAchievements);
+
 
 router.get("/professional", CommonController.getAllDoctor);
 
